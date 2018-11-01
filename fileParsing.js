9bb92968dfdb5b409557903381732c3f748c7deb
vanthanh <vanthanh971125@gmail.com>
2018-11-01 20:41:06 +0700

    update fileParsing

--- fileParsing.js
+++ fileParsing.js
@@ -15,144 +15,143 @@ var agentCollection = "%s_%s";
 var ossecDir = "/var/ossec";
 var filesAndDir = [ossecDir + "/etc/client.keys", ossecDir + "/queue/agent-info/"];
 
-var watcher = chokidar.watch(filesAndDir, {persistent: true});
+async function main() {
+  var watcher = chokidar.watch(filesAndDir, {persistent: true});
 
-watcher
-  .on('add', function(fPath, stat) {
-    console.log("File ", fPath, " has been added")
-    if (fPath.indexOf("client.keys") > -1) {
-      fs.readFile(fPath, (err, data) => {
-        if (!err) {
-          mongoose.connect(url + "/" + globalCollection);
-          mongoose.Promise = global.Promise;
-          var db = mongoose.connection;
-          db.on('error', err => console.log("Connection Error: " + err));
-          db.once('open', () => {
-            console.log(db);
-            const Schema = mongoose.Schema;
-            var agentInfoSchema = Schema({
-              id: String,
-              name: String,
-              ip: String,
-              key: String,
-              os: String,
-              os_arch: String,
-              version: String,
-              config_sum: String,
-              merge_sum: String,
-              lastAlive: Date
+  watcher
+    .on('add', function(fPath, stat) {
+      console.log("File ", fPath, " has been added")
+      if (fPath.indexOf("client.keys") > -1) {
+        fs.readFile(fPath, (err, data) => {
+          if (!err) {
+            mongoose.connect(url + "/" + globalCollection);
+            mongoose.Promise = global.Promise;
+            var db = mongoose.connection;
+
+            db.on('error', err => console.log("Connection Error: " + err));
+            db.once('open', () => {
+              const Schema = mongoose.Schema;
+              var agentInfoSchema = Schema({
+                id: String,
+                name: String,
+                ip: String,
+                key: String,
+                os: String,
+                os_arch: String,
+                version: String,
+                config_sum: String,
+                merge_sum: String,
+                lastAlive: Date
+              });
+
+              agentInfoSchema.index({"id": 1, "name": 1});
+
+              var agentInfoModel = mongoose.model("agent", agentInfoSchema, "agent");
+              
+              var lines = data.toString().trim().split("\n");
+
+              var validAgentList = [];
+
+              var addDocsToCollection = (line) => {
+                var agentInfoArray = line.split(" ");
+                var agentObj = new agentInfoModel({
+                  id: agentInfoArray[0].toString().trim(),
+                  name: agentInfoArray[1].toString().trim(),
+                  ip: agentInfoArray[2].toString().trim(),
+                  hashed: agentInfoArray[3].toString().trim(),
+                });
+                agentInfoModel.findOneAndUpdate({
+                  id: agentInfoArray[0].toString().trim(),
+                  name: agentInfoArray[1].toString().trim()},
+                  agentObj, {new: true, upsert: true})
+                  .then((fuErr, fuRes) => {
+                    if (fuErr) {
+                      console.log("Find & Update Error: " + fuErr);
+                    } else {
+                      validAgentList.push(fuRes);
+                    }
+                })
+              }
+
+              const asyncDo = (value, cb) => {
+                cd(line);
+              }
+
+              var doAddDocsToCollection = (line) => {
+                asyncDo(line, addDocsToCollection);
+              }
+
+              lines.forEach(doAddDocsToCollection);
+
+              // delete agents not in client.keys
+              agentInfoModel.find({}).then((agentList) => {
+                agentList.forEach((agent) => {
+                  var agentInfo = {}
+                  if (!(agent in validAgentList)) {
+                    agentInfoModel.deleteOne(agent);
+                  }
+                })
+              }).then(() => {
+                mongoose.connection.close();
+              })
             })
-
-            var agentInfoModel = mongoose.model("agent", agentInfoSchema, "agent");
-
-            console.log(agentInfoModel.collection);
-
-            agentInfoModel.collection.indexes((iErr, indexes) => {
-              if (iErr) console.log(iErr)
-              else console.log(indexes);
-            })
-            agentInfoModel.find((fErr, fRes) => {
-              // fRes.forEach((value, index, array) => {
-              //   console.log(value);
-              //   console.log(index);
-              //   console.log(array);
-              //   console.log();
-              // })
-              if (fErr) console.log(fErr)
-              else console.log(fRes);
-            })
+            }
           })
-          // mongoClient.connect(url, {poolSize: 10}, (connectErr, db) => {
-          //   if (connectErr) {
-          //     console.log("Unable to connect to database");
-          //   } else { 
-          //     var dbo = db.db(globalCollection);
-          //     dbo.createCollection("agent", (createColError, createColRes) => {
-          //       // console.log(createColError);
-          //       // console.log(createColRes);
-          //       // db.close();
-          //     })
-          //     var managerObj = {
-          //       id: "000",
-          //     }
+      } else if (fPath.indexOf("/queue/agent-info/") > -1) {
+        var fPathArray = fPath.split("/");
+        var fName = fPathArray[fPathArray.length - 1];
+        var id = fName.split("_")[0];
+        var name = fName.split("_")[1];
 
-          //     dbo.collection("agent").findOne(managerObj, (findErr, findRes) => {
-          //       if (findErr == null & findRes == null) {
-          //         dbo.collection("agent").insertOne(managerObj, (insertErr,  insertRes) => {
-          //           if (insertErr) {
-          //             console.log(insertErr);
-          //           } else {
-          //             console.log(insertRes);
-          //           }
-          //           // db.close();
-          //         })
-          //       }
-          //       // db.close();
-          //     })
+        
+        mongoose.connect(url + "/" + globalCollection);
+        mongoose.Promise = global.Promise;
 
-          //     dbo.collection("agent").createIndex({"id":  1}, {unique: true}, (indexErr, indexRes) => {
-          //       if (indexErr) {
-          //         console.log(indexErr);
-          //       } else {
-          //         console.log(indexRes);
-          //       }
-          //       // db.close();
-          //     })
+        var db = mongoose.connection;
+        db.on("error", err => console.log("Connection Error: " + err));
+        db.on("open", () => {
+          const Schema = mongoose.Schema;
+          var agentInfoSchema = Schema({
+            id: String,
+            name: String,
+            ip: String,
+            key: String,
+            os: String,
+            os_arch: String,
+            version: String,
+            config_sum: String,
+            merge_sum: String,
+            lastAlive: Date
+          });
 
-          //     var lines = data.toString().trim().split("\n");
-          //     lines.forEach(line => {
-          //       if (!(/^\s*$/.test(line))) {
-          //         var agentInfoArray = line.split(" ");
-          //         var agentObj = {
-          //           id: agentInfoArray[0].toString().trim(),
-          //           name: agentInfoArray[1].toString().trim(),
-          //           ip: agentInfoArray[2].toString().trim(),
-          //           hashed: agentInfoArray[3].toString().trim(),
-          //         }
+          agentInfoSchema.index({"id": 1, "name": 1});
 
-          //         dbo.collection("agent").insertOne(agentObj, (insErr, insRes) => {
-          //           if (insErr) {
-          //             var updateObj = { $set: {
-          //               name: agentInfoArray[1].toString().trim(),
-          //               ip: agentInfoArray[2].toString().trim(),
-          //               hashed: agentInfoArray[3].toString().trim(),
-          //             }}
-          //             dbo.collection("agent").updateOne({id: agentInfoArray[0].toString().trim()}, updateObj, (updErr, updRes) => {
-          //               if (updErr) {
-          //                 console.log("Update Error");
-          //                 console.log(updErr);
-          //               } else {
-          //                 console.log("Updated");
-          //                 console.log(updRes);
-          //               }
-          //               // db.close();
-          //             })
-          //           } else {
-          //             console.log("Inserted");
-          //             console.log(insRes);
-          //           }
-          //           // db.close();
-          //         })
-          //       }
-          //     })
-          //     db.close();
-          //   }
-          // })
+          var agentInfoModel = mongoose.model("agent", agentInfoSchema, "agent");
 
-          }
+          // get agent info from file
+          
+          var agentInfo = new agentInfoModel({
+            id: id,
+            name: name,
+          })
+
+
+          
         })
-    } else if (fPath.indexOf("/queue/agent-info/") > -1) {
 
-    } else if (fPath.indexOf("/queue/rootcheck/") > -1) {
+      } else if (fPath.indexOf("/queue/rootcheck/") > -1) {
 
-    } else if (fPath.indexOf("/queue/syscheck/") > -1) {
+      } else if (fPath.indexOf("/queue/syscheck/") > -1) {
 
-    } else {
-      console.log("Something's not right");
-    }
-  })
-  .on('change', function(fPath, stat) {
-    // console.log('File', path, 'has been changed');
-  })
-  .on('unlink', function(fPath) {console.log('File', path, 'has been removed');})
-  .on('error', function(error) {console.error('Error happened', error);})
+      } else {
+        console.log("Something's not right");
+      }
+    })
+    .on('change', function(fPath, stat) {
+      // console.log('File', path, 'has been changed');
+    })
+    .on('unlink', function(fPath) {console.log('File', path, 'has been removed');})
+    .on('error', function(error) {console.error('Error happened', error);})
+}
+
+main()
